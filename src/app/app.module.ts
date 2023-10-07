a77ccb557f70ce78dc55397d5e6da57c46df15fe
lairase <Ovidio.LAIRANA-FRANCO@ext.ec.europa.eu>
2023-10-07 11:49:41 +0200

    misc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RxjsExampleModule } from './rxjs-example/rxjs-example.module';
+import { LibraryExampleModule } from './library-example/library-example.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RxjsExampleModule,
+    LibraryExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
